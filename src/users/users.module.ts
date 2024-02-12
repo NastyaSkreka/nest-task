5bd5bc196b45d48f46078aba6999d982ff807938
NastyaSkreka <skreka.nastya@gmail.com>
2024-02-12 10:23:26 +0200

    feat: add auth guard

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,15 +1,20 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserModel } from './users.model';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
   controllers: [UsersController],
   providers: [UsersService], 
   imports: [
+    forwardRef(() => AuthModule),
     MongooseModule.forFeature([{ name: User.name, schema: UserModel }])
   ], 
-  exports: [UsersService]
+  exports: [
+    UsersService, 
+
+]
 })
 export class UsersModule {}
