5396d07dfed766b625682857993316ca80579ff0
NastyaSkreka <skreka.nastya@gmail.com>
2024-02-12 09:41:30 +0200

    feat: add login function

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable, UnauthorizedException } from '@nestjs/common';
 import { CreateUserDto } from 'src/users/dto/create-user.dto';
 import { UsersService } from '../users/users.service'
 import { JwtService } from '@nestjs/jwt';
@@ -12,7 +12,8 @@ export class AuthService {
                 private jwtService: JwtService) {}
 
    async login(userDto: CreateUserDto) {
-        
+        const user = await this.validateUser(userDto)
+        return this.generateToken(user)
     }
 
 
@@ -26,10 +27,19 @@ export class AuthService {
         return this.generateToken(user)
     }
 
-    async generateToken(user: User) {
+    private async generateToken(user: User) {
         const payload = { email: user.email, id: user._id }
         return { 
             token: this.jwtService.sign(payload)
         }
+    }
+
+    private async validateUser(userDto: CreateUserDto) {
+        const user = await this.usersService.getUserByEmail(userDto.email);
+        const passwordEquals = await bcrypt.compare(userDto.password, user.password);
+        if (user && passwordEquals) {
+            return user;
+        }
+        throw new UnauthorizedException({message: 'Некорректный емайл или пароль' })
     }
 }
